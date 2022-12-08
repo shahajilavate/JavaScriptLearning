d0775cc6ce7840358daa2171f3e0dd9135e99f93
shahajilavate <shahajilavate431@gmail.com>
2022-12-08 10:40:48 +0530

    01_vaiable_assign.js
    01_vaiable_assign.js
    hello html,hellojs

---
+++ 01_variable_assign.js
@@ -0,0 +1,33 @@
+var myFullName;
+console.log(myFullName);
+
+myFullName = "shahaji lavate";
+console.log(myFullName);
+
+myFullName = "Shahaji Ramchandra Lavate";
+console.log(myFullName);
+
+var myLove = "I Love Only JS";
+console.log(myLove);
+
+var firstName ="Shahaji";
+
+
+var lastName ="lavate";
+
+
+var age ="25";
+
+
+var address ="Sangola";
+
+
+var pin_Code ="413309";
+
+
+var gender ="Male";
+
+
+var is_married ="NO";
+
+console.log(firstName,lastName,age,address,pin_Code,gender,is_married);
