00825d05e7670d21170be8e0c640d950b977c716
shahajilavate <shahajilavate431@gmail.com>
2022-12-09 09:32:54 -0500

    01_typeOf.js
    01_typeOf.js

---
+++ 01_typeof.js
@@ -0,0 +1,41 @@
+console.log(
+    "===========================Assignment_2==========================="
+  );
+var bankname = "SBI Bank";
+console.log(bankname);
+var banknametype= typeof bankname;
+console.log(banknametype);
+console.log(
+    "================================================================="
+  );
+ var marks = '90%'
+ console.log(marks);
+ var markstype = typeof marks;
+ console.log(markstype);
+ console.log(
+    "================================================================="
+  );
+  var is_working = true;
+  console.log(is_working);
+  var is_workingtype = typeof is_working;
+  console.log(is_workingtype);
+  console.log(
+    "================================================================="
+  );
+  var totalcount = "Hundred and seven";
+  console.log(totalcount);
+  var totalcounttype = typeof totalcount;
+  console.log(totalcounttype);
+  console.log(
+    "================================================================="
+  );
+  var totalcount = 107;
+  console.log(totalcount);
+  var totalcounttype = typeof totalcount;
+  console.log(totalcounttype);
+  console.log(
+    "================================================================="
+  );
+
+  
+  
